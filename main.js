4648d4c128d288a6450e03b400a40c80ce38f3c4
Heather <lilpinkrose02@gmail.com>
2023-03-10 22:21:40 -0600

    created popup layout, trying to solve a scrolling problem in popup.

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const tabsContainer = document.querySelector('.about-tabs')
-let aboutSection = document.querySelector('.about-section')
-
+const tabsContainer = document.querySelector('.about-tabs');
+let aboutSection = document.querySelector('.about-section');
 
 tabsContainer.addEventListener('click', (e) => {
-    if (e.target.classList.contains('tab-item') && !e.target.classList.contains('active')) {
-        tabsContainer.querySelector('.active').classList.remove('active')
-        e.target.classList.add('active')
+  if (e.target.classList.contains('tab-item') && !e.target.classList.contains('active')) {
+    tabsContainer.querySelector('.active').classList.remove('active');
+    e.target.classList.add('active');
 
-        const target = e.target.getAttribute('data-target')
+    const target = e.target.getAttribute('data-target');
 
-        aboutSection.querySelector('.tab-content.active').classList.remove('active')
-        aboutSection.querySelector(target).classList.add('active')
-    }
+    aboutSection.querySelector('.tab-content.active').classList.remove('active');
+    aboutSection.querySelector(target).classList.add('active');
+  }
+});
+
+// ======================== project popup =========
+
+const projectBoxes = Array.from(document.querySelectorAll('.project'))
+
+projectBoxes.forEach(projectBox => {
+    projectBox.addEventListener('click', () => {
+        let projectPopup = projectBox.querySelector('.project-popup-wrap')
+        projectPopup.classList.add('active')
+    })
 })
